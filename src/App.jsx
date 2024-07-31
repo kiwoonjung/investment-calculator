072cff82ffb202cb678a90936602a84a428388ec
kiwoonjung <kiwoon0627@gmail.com>
2024-07-31 00:21:25 -0700

    20240730 00:20 Outputting Results in a List & Deriving More Values

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Results from "./components/Results.jsx";
 
 function App() {
   const [userInput, setUserInput] = useState({
-    intialInvestment: 10000,
+    initialInvestment: 10000,
     annualInvestment: 1200,
     expectedReturn: 6,
     duration: 10,
@@ -16,7 +16,7 @@ function App() {
     setUserInput((prevUserInput) => {
       return {
         ...prevUserInput,
-        [inputIdentifier]: newValue,
+        [inputIdentifier]: +newValue,
       };
     });
   }
